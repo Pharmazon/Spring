daa89d76afe3d4a80fa3017486889e7f9a3f0f15
Pharmazon <as.shcheglov@gmail.com>
2018-12-04 01:32:34 +0400

    Added Main class

--- NewsPortal/src/main/java/ru/shcheglov/geekbrains/lessons/lesson3/Main.java
+++ NewsPortal/src/main/java/ru/shcheglov/geekbrains/lessons/lesson3/Main.java
@@ -1,5 +1,14 @@
 package ru.shcheglov.geekbrains.lessons.lesson3;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.shcheglov.geekbrains.lessons.lesson3.config.ApplicationConfiguration;
+import ru.shcheglov.geekbrains.lessons.lesson3.dao.AuthorDAO;
+import ru.shcheglov.geekbrains.lessons.lesson3.dao.DAO;
+import ru.shcheglov.geekbrains.lessons.lesson3.entity.Author;
+
+import java.util.List;
+
 /**
  * @author Alexey Shcheglov
  * @version dated 02.12.2018
@@ -8,6 +17,24 @@ package ru.shcheglov.geekbrains.lessons.lesson3;
 public class Main {
 
     public static void main(String[] args) {
+        final ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        final AuthorDAO authorDAO = context.getBean(AuthorDAO.class);
 
+        Author author2 = new Author();
+        author2.setFirstName("Ivan");
+        author2.setLastName("Petrov");
+        author2.setEmail("q@mail.ru");
+        authorDAO.create(author2);
+
+        Author author1 = new Author();
+        author1.setFirstName("Olesya");
+        author1.setLastName("Shcheglova");
+        author1.setEmail("dffff@mail.ru");
+        authorDAO.create(author1);
+
+        List<Author> list = authorDAO.readAll();
+        for (Author author : list) {
+            System.out.println(author);
+        }
     }
 }
