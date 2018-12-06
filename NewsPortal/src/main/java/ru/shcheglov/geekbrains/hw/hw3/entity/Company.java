fdd13b46605f6a88396ddba8c70a0707e076e809
Pharmazon <as.shcheglov@gmail.com>
2018-12-06 15:40:25 +0400

    Associated fields excluded from toString

--- NewsPortal/src/main/java/ru/shcheglov/geekbrains/hw/hw3/entity/Company.java
+++ NewsPortal/src/main/java/ru/shcheglov/geekbrains/hw/hw3/entity/Company.java
@@ -2,6 +2,7 @@ package ru.shcheglov.geekbrains.hw.hw3.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@ToString(exclude = "ads")
 @Entity
 @Table(name = "company")
 public class Company extends AbstractEntity {
