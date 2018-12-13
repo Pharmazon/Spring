f4cac73dbb95715d0c454d4400defd580d9674d0
Pharmazon <as.shcheglov@gmail.com>
2018-12-14 00:00:52 +0400

    Models added

---
+++ ProjectPortal/src/main/java/ru/shcheglov/model/AbstractEntity.java
@@ -0,0 +1,24 @@
+package ru.shcheglov.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.UUID;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 13.12.2018
+ */
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @Column(name = "id")
+    private String id = UUID.randomUUID().toString();
+
+    public String getId() {
+        return id;
+    }
+
+}
---
+++ ProjectPortal/src/main/java/ru/shcheglov/model/Category.java
@@ -0,0 +1,45 @@
+package ru.shcheglov.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 13.12.2018
+ */
+
+@Entity
+@Table(name = "category")
+@NamedQueries({
+        @NamedQuery(name = "Category.findAll", query = "SELECT a FROM Category a"),
+        @NamedQuery(name = "Category.deleteAll", query = "DELETE FROM Category a")
+})
+public class Category extends AbstractEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Ad> ads;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ad> getAds() {
+        return ads;
+    }
+
+    public void setAds(List<Ad> ads) {
+        this.ads = ads;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
