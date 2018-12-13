328889b05a7fad138f40d445bde6fbbece2ef16c
Pharmazon <as.shcheglov@gmail.com>
2018-12-14 00:01:12 +0400

    SpringConfiguration added

---
+++ ProjectPortal/src/main/java/ru/shcheglov/config/WebMvcConfig.java
@@ -0,0 +1,46 @@
+package ru.shcheglov.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 13.12.2018
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.shcheglov.controller")
+@Import(DataSourceConfiguration.class)
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver resolver() {
+        final InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        final ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+
+}
