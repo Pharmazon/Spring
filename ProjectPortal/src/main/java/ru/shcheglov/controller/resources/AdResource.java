4e0d73af00303f819d9a7c437e571731ab7635e7
Pharmazon <as.shcheglov@gmail.com>
2018-12-14 15:05:41 +0400

    Added AdsResource and AdResource RestControllers

---
+++ ProjectPortal/src/main/java/ru/shcheglov/controller/resources/AdResource.java
@@ -0,0 +1,48 @@
+package ru.shcheglov.controller.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.shcheglov.dto.AdDTO;
+import ru.shcheglov.dto.ResultDTO;
+import ru.shcheglov.model.Ad;
+import ru.shcheglov.service.AdService;
+
+import java.util.Optional;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 14.12.2018
+ */
+
+@RestController
+@RequestMapping("api/ad")
+public class AdResource {
+
+    @Autowired
+    private AdService adService;
+
+    @GetMapping(value = "{id}", produces = "application/json")
+    public AdDTO get(@PathVariable("id") final String id) {
+        final Optional<Ad> ad = adService.get(id);
+        return ad.map(AdDTO::new).orElse(null);
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO post(final AdDTO adDTO) {
+        adService.save(adDTO);
+        return new ResultDTO();
+    }
+
+    @PutMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO put(final AdDTO adDTO) {
+        adService.save(adDTO);
+        return new ResultDTO();
+    }
+
+    @DeleteMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO delete(final AdDTO adDTO) {
+        adService.delete(adDTO);
+        return new ResultDTO();
+    }
+
+}
---
+++ ProjectPortal/src/main/java/ru/shcheglov/controller/resources/AdsResource.java
@@ -0,0 +1,52 @@
+package ru.shcheglov.controller.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.shcheglov.dto.AdDTO;
+import ru.shcheglov.dto.ResultDTO;
+import ru.shcheglov.model.Ad;
+import ru.shcheglov.service.AdService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 14.12.2018
+ */
+
+@RestController
+@RequestMapping("api/ads")
+public class AdsResource {
+
+    @Autowired
+    private AdService adService;
+
+    @GetMapping(produces = "application/json")
+    public List<AdDTO> get() {
+        final List<Ad> ads = adService.getAll();
+        return StreamSupport.stream(ads.spliterator(), false)
+                .map(AdDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO post(final AdDTO[] ads) {
+        for (final AdDTO ad : ads) adService.save(ad);
+        return new ResultDTO();
+    }
+
+    @PutMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO put(final AdDTO[] ads) {
+        for (final AdDTO ad : ads) adService.save(ad);
+        return new ResultDTO();
+    }
+
+    @DeleteMapping(produces = "application/json", consumes = "application/json")
+    public ResultDTO delete(final AdDTO[] ads) {
+        for (final AdDTO ad : ads) adService.delete(ad);
+        return new ResultDTO();
+    }
+
+}
