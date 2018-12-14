8eb145bfb53d565e7febad928f2b3f5ae51a33db
Pharmazon <as.shcheglov@gmail.com>
2018-12-14 15:05:54 +0400

    Added Simple RestController

--- ProjectPortal/src/main/java/ru/shcheglov/controller/rest/SimpleREST.java
+++ ProjectPortal/src/main/java/ru/shcheglov/controller/rest/SimpleREST.java
@@ -1,6 +1,7 @@
 package ru.shcheglov.controller.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.shcheglov.dto.ResultDTO;
@@ -17,6 +18,13 @@ public class SimpleREST {
 
     @GetMapping(value = "pingJSON", produces = "application/json")
     public ResultDTO pingJSON() {
+        return new SuccessDTO();
+    }
+
+    @GetMapping(value = "pingJSONKey/{apiKey}", produces = "application/json")
+    public ResultDTO pingJSONKey(
+            @RequestHeader("apiKey") final String key
+    ) {
         return new SuccessDTO();
     }
 
