00e6f757659bbd8f342e5733d898d2f9880ed983
Pharmazon <as.shcheglov@gmail.com>
2018-12-16 15:34:08 +0400

    CategoryController added

---
+++ ProjectPortal/src/main/java/ru/shcheglov/controller/web/CategoryController.java
@@ -0,0 +1,74 @@
+package ru.shcheglov.controller.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.shcheglov.model.Category;
+import ru.shcheglov.service.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Alexey Shcheglov
+ * @version dated 15.12.2018
+ */
+
+@Controller
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("category-list")
+    public String categoryList(final Model model) {
+        final List<Category> categories = categoryService.getAll();
+        model.addAttribute("categories", categories);
+        return "category-list";
+    }
+
+    @GetMapping("category-add")
+    public String categoryAdd() {
+        return  "category-add";
+    }
+
+    @PostMapping("category-create")
+    public String categoryCreate(final Category category, final Model model, final BindingResult result) {
+        if (!result.hasErrors()) {
+            model.addAttribute("category", category);
+            categoryService.save(category);
+        }
+        return "redirect:/category-list";
+    }
+
+    @PostMapping("category-save")
+    public String categorySave(@ModelAttribute("company") final Category category, final BindingResult result) {
+        if (!result.hasErrors()) categoryService.update(category);
+        return "redirect:/category-list";
+    }
+
+    @GetMapping("category-edit/{id}")
+    public String categoryEdit(final Model model, @PathVariable("id") final String id) {
+        final Optional<Category> category = categoryService.get(id);
+        category.ifPresent(cat -> model.addAttribute("category", cat));
+        return "category-edit";
+    }
+
+    @GetMapping("category-view/{id}")
+    public String categoryView(final Model model, @PathVariable("id") final String id) {
+        final Optional<Category> category = categoryService.get(id);
+        category.ifPresent(a -> model.addAttribute("category", a));
+        return "category-view";
+    }
+
+    @GetMapping("category-delete/{id}")
+    public String categoryDelete(@PathVariable("id") final String id) {
+        categoryService.delete(id);
+        return "redirect:/category-list";
+    }
+}
