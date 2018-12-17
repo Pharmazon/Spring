e91576bfbbbc4717057f1d780691e7ec01f929ff
Pharmazon <as.shcheglov@gmail.com>
2018-12-18 00:32:24 +0400

    added findAllAds

--- ProjectPortal/src/main/java/ru/shcheglov/repository/CompanyRepository.java
+++ ProjectPortal/src/main/java/ru/shcheglov/repository/CompanyRepository.java
@@ -3,6 +3,8 @@ package ru.shcheglov.repository;
 import ru.shcheglov.model.Ad;
 import ru.shcheglov.model.Company;
 
+import java.util.List;
+
 /**
  * @author Alexey Shcheglov
  * @version dated 13.12.2018
@@ -11,5 +13,7 @@ import ru.shcheglov.model.Company;
 public interface CompanyRepository extends BasicRepository<Company> {
 
     Company findOneByAd(Ad ad);
+
+    List<Ad> findAllAds(Company model);
 
 }
