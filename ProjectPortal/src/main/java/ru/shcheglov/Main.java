61a348754d909758ea56da029e4b7650ca249a9f
Pharmazon <as.shcheglov@gmail.com>
2018-12-18 00:31:39 +0400

    Minor refactorings

--- ProjectPortal/src/main/java/ru/shcheglov/Main.java
+++ ProjectPortal/src/main/java/ru/shcheglov/Main.java
@@ -3,10 +3,15 @@ package ru.shcheglov;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.shcheglov.config.ApplicationConfiguration;
 import ru.shcheglov.model.Ad;
+import ru.shcheglov.model.Category;
+import ru.shcheglov.model.Company;
+import ru.shcheglov.repository.CategoryRepository;
+import ru.shcheglov.repository.CompanyRepository;
 import ru.shcheglov.service.AdService;
 import ru.shcheglov.service.CategoryService;
 import ru.shcheglov.service.CompanyService;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -22,7 +27,17 @@ public class Main {
         final CategoryService categoryService = context.getBean("categoryService", CategoryService.class);
         final CompanyService companyService = context.getBean("companyService", CompanyService.class);
 
-        Optional<Ad> ad = adService.get("303d56ae-57ef-4147-8ffc-10b8054dd72e");
-        System.out.println(ad.get());
+        final CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        final CategoryRepository categoryRepository = context.getBean("categoryRepository", CategoryRepository.class);
+
+        Company company = companyService.get("92d890ff-4031-4457-86f0-1630b537d6f6").get();
+        Category category = categoryService.get("76801e71-95ff-4ac3-836a-3f3291bff254").get();
+
+        List all = categoryRepository.findAllAds(category);
+        List ads = companyRepository.findAllAds(company);
+        all.forEach(System.out::println);
+        System.out.println("-----------------------------");
+        ads.forEach(System.out::println);
+
     }
 }
