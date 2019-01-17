1c743b578824d4109abd695309028e97098fd92c
Pharmazon <as.shcheglov@gmail.com>
2019-01-18 01:06:37 +0300

    Entities for JPA configured

--- second/src/main/java/ru/shcheglov/spring/second/model/Task.java
+++ second/src/main/java/ru/shcheglov/spring/second/model/Task.java
@@ -1,30 +1,36 @@
 package ru.shcheglov.spring.second.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.jetbrains.annotations.Nullable;
+
+import javax.persistence.*;
 
 /**
  * @author Alexey Shcheglov
  * @version dated 16.01.2019
  */
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "app_tasks")
+@EqualsAndHashCode(callSuper = true)
 public class Task extends AbstractEntity {
 
     @Nullable
+    @Column(name = "name")
     private String name = "";
 
     @Nullable
+    @Column(name = "description")
     private String description = "";
 
     @Nullable
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id")
     private Person person = null;
 
     public Task(@Nullable final String name) {
