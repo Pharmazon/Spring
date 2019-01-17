7eff2d215bda6fb905fb67c2ec042591cecd9ccb
Pharmazon <as.shcheglov@gmail.com>
2019-01-17 22:36:05 +0300

    Minor refactorings

--- second/src/test/java/ru/shcheglov/spring/second/SecondApplicationTests.java
+++ second/src/test/java/ru/shcheglov/spring/second/SecondApplicationTests.java
@@ -1,14 +1,9 @@
 package ru.shcheglov.spring.second;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.shcheglov.spring.second.model.Person;
-import ru.shcheglov.spring.second.repository.PersonRepository;
 
 /**
  * @author Alexey Shcheglov
@@ -18,26 +13,6 @@ import ru.shcheglov.spring.second.repository.PersonRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SecondApplicationTests {
-
-	@Autowired
-	private PersonRepository personRepository;
-
-	private Person actualPerson;
-
-	@Before
-	public void init() {
-		this.actualPerson = new Person("Сидр", "Иванов", "Иванович", "ivan@ivanov.com");
-		personRepository.merge(actualPerson);
-	}
-
-	@Test
-	public void whenUpdatePersonThenEquals() {
-		final String addedId = actualPerson.getId();
-		final Person expectedPerson = personRepository.findOneById(addedId);
-		expectedPerson.setFirstName("Alexey");
-		personRepository.merge(expectedPerson);
-		Assert.assertEquals(expectedPerson, actualPerson);
-	}
 
 	@Test
 	public void contextLoads() {
